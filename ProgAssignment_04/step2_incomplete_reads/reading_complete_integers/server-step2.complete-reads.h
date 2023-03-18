cf39c488ae1cf32b794e744486e8897ccbe7c754
Goofball <asajh4@gmail.com>
2023-03-17 17:25:08 -0700

    Created the reading_complete-integers directory and copied client and server code to the directory in preparation for implementing the read_int() function to ensure integers are completely read from a socket.

---
+++ ProgAssignment_04/step2_incomplete_reads/reading_complete_integers/server-step2.complete-reads.h
@@ -0,0 +1,25 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <syslog.h>
+
+/* Function prototypes */
+void* handle_client(void* arg);
+
+/* Preprocessor directives */
+#define SERVER_ADDR "127.0.0.1" // loopback ip address
+#define PORT 23657              // port the server will listen on
+
+#define FALSE 0
+#define TRUE !FALSE
+
+#define NUM_CONNECTIONS 5       // number of pending connections in the connection queue
+
