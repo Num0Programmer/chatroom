c5fb3fc315dfe8cfaaa69a51a1efa7a599e4df44
zksx <52799997+zksx@users.noreply.github.com>
2023-04-05 13:42:26 -0700

    dbg.h file works

--- ProgAssignment_05/aux/properties.c
+++ ProgAssignment_05/aux/properties.c
@@ -6,7 +6,7 @@
 
 
 #define DBG
-//#include "dbg.h"
+#include "dbg.h"
 
 #include "properties.h"
 
@@ -145,6 +145,12 @@ int main(int argc, const char * argv[]) {
     properties = property_read_properties(properties_file);
     value = property_get_property(properties, key);
     
+    printf("\nValue for %s: %s\n", key, value);
+
+    key = "IP";
+
+    properties = property_read_properties(properties_file);
+    value = property_get_property(properties, key);
     printf("\nValue for %s: %s\n", key, value);
 
     return EXIT_SUCCESS;
