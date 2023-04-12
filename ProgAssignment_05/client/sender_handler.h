c462957e410eba2b94422149e0c9bd0ed1e24416
zksx <52799997+zksx@users.noreply.github.com>
2023-04-12 12:56:19 -0700

    added support to conneect to local server

--- ProgAssignment_05/client/sender_handler.h
+++ ProgAssignment_05/client/sender_handler.h
@@ -4,13 +4,20 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
 
 #include "../chat_node.h"
 #include "../message.h"
 
+#define LOCAL_SERVER_ADDR "127.0.0.1"
+#define LOCAL_SERVER_PORT 23657
 
 #ifndef SENDER_HANDLER_H
 #define SENDER_HANDLER_H
+
+
 
 
 /* structures */
